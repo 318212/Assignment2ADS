1c392b00df64dc4c066a461dfff32cb6d060d30b
JuanVM1987 <1987jvm@gmail.com>
2023-03-18 00:22:36 +0100

    check remove

--- src/main/java/classes/main.java
+++ src/main/java/classes/main.java
@@ -18,14 +18,18 @@ public class main {
         BinaryTreeNode<Integer> b6 = new BinaryTreeNode<>(19);
         BinaryTreeNode<Integer> b7 = new BinaryTreeNode<>(25);
         BinaryTreeNode<Integer> bTest = new BinaryTreeNode<>(100);
-/*        bt.setRoot(new BinaryTreeNode<>(10));
+        /*
+        bt.setRoot(new BinaryTreeNode<>(10));
         bt.getRoot().addLeftChild(b2);
         bt.getRoot().addRightChild(b3);
         b2.addLeftChild(b4);
         b2.addRightChild(b5);
         b3.addLeftChild(b6);
         b3.addRightChild(b7);
-        b7.addRightChild(bTest);*/
+        b7.addRightChild(bTest);
+        */
+
+
         BinarySearchTree<Integer> searchTree= new BinarySearchTree<>();
         searchTree.insert(b2.getElement());
         searchTree.insert(b3.getElement());
@@ -37,6 +41,7 @@ public class main {
         print.printTree(searchTree.getRoot());
         System.out.println();
         searchTree.removeElement(11);
+
         print.printTree(searchTree.getRoot());
         System.out.println();
         System.out.println(bt.inOrder());
@@ -45,6 +50,6 @@ public class main {
         System.out.println();
         System.out.println(bt.postOrder());
         System.out.println();
-       // System.out.println(bt.height()); //todo heigth not stop if it is empty
+       // System.out.println(bt.height()); //todo height not stop if it is empty
     }
 }
