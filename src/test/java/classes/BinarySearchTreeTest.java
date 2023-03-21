ba45da84860a13f2c766a4c89ce36d0fef71c59b
Franciszek Nurkiewicz <318212@via.dk>
2023-03-21 14:36:17 +0100

    cleaning

--- src/test/java/classes/BinarySearchTreeTest.java
+++ src/test/java/classes/BinarySearchTreeTest.java
@@ -108,20 +108,4 @@ class BinarySearchTreeTest {
         int maxValueFromTheTree = bst.findMax();
         assertEquals(25, maxValueFromTheTree);
     }
-
-    @Test
-    public void BST_rebalanceForXElementTree(){
-        bst.insert(root.getElement());
-        bst.insert(b2.getElement());
-        bst.insert(b3.getElement());
-        bst.insert(b4.getElement());
-        bst.insert(b5.getElement());
-        bst.insert(b6.getElement());
-        bst.insert(b7.getElement());
-        print.printTree(bst.getRoot());
-        bst.rebalance();
-        System.out.println();
-        print.printTree(bst.getRoot());
-
-    }
 }
